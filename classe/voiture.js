aa11efc59e55e97d9db0d4fa8274ceccb0335740
Rogel000 <roge.lucie.dev@gmail.com>
2024-08-23 16:27:16 +0200

    exercice 27

---
+++ classe/voiture.js
@@ -0,0 +1,15 @@
+export default class Voiture {
+  constructor(marque, modele, vitesse) {
+    this.marque = marque;
+    this.modele = modele;
+    this.vitesse = vitesse;
+  }
+
+  accelerer() {
+    this.vitesse += 10;
+  }
+
+  tourner() {
+    this.vitesse -= 5;
+  }
+}
